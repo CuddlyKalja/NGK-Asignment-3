0531a881def692702e0d181b0fccb6dbf4b7e13e
CuddlyKalja <farpeja2002@gmail.com>
2025-05-14 09:29:58 +0200

    Update main.cpp

--- Assignment-4/main.cpp
+++ Assignment-4/main.cpp
@@ -6,41 +6,35 @@
 #include <algorithm>
 #include <map>
 
+namespace rr = restinio::router;
+using router_t = rr::express_router_t<>;
+
+using traits_t = restinio::traits_t<
+    restinio::asio_timer_manager_t,
+    restinio::single_threaded_ostream_logger_t,
+    router_t>;
+
 namespace rws = restinio::websocket::basic;
 
+// ----------------- Vejrdata struktur -----------------
 struct weatherRegistration {
     weatherRegistration() = default;
 
-    weatherRegistration(
-        int ID,
-        int Date,
-        int Time,
-        std::string placeName,
-        double Lat,
-        double Lon,
-        double Temperature,
-        int Humidity)
-        : m_id{ ID }
-        , m_date{ Date }
-        , m_time{ Time }
-        , m_placeName{ std::move(placeName) }
-        , m_lat{ Lat }
-        , m_lon{ Lon }
-        , m_temperature{ Temperature }
-        , m_humidity{ Humidity }
-    {}
+    weatherRegistration(int ID, int Date, int Time, std::string placeName,
+                        double Lat, double Lon, double Temperature, int Humidity)
+        : m_id{ ID }, m_date{ Date }, m_time{ Time }, m_placeName{ std::move(placeName) },
+          m_lat{ Lat }, m_lon{ Lon }, m_temperature{ Temperature }, m_humidity{ Humidity } {}
 
-    template < typename JSON_IO >
+    template <typename JSON_IO>
     void json_io(JSON_IO &io) {
-        io
-            & json_dto::mandatory("ID", m_id)
-            & json_dto::mandatory("Date", m_date)
-            & json_dto::mandatory("Time", m_time)
-            & json_dto::mandatory("PlaceName", m_placeName)
-            & json_dto::mandatory("Lat", m_lat)
-            & json_dto::mandatory("Lon", m_lon)
-            & json_dto::mandatory("Temperature", m_temperature)
-            & json_dto::mandatory("Humidity", m_humidity);
+        io & json_dto::mandatory("ID", m_id)
+           & json_dto::mandatory("Date", m_date)
+           & json_dto::mandatory("Time", m_time)
+           & json_dto::mandatory("PlaceName", m_placeName)
+           & json_dto::mandatory("Lat", m_lat)
+           & json_dto::mandatory("Lon", m_lon)
+           & json_dto::mandatory("Temperature", m_temperature)
+           & json_dto::mandatory("Humidity", m_humidity);
     }
 
     int m_id;
@@ -54,13 +48,11 @@ struct weatherRegistration {
 };
 
 using weatherStation_t = std::vector<weatherRegistration>;
-namespace rr = restinio::router;
-using router_t = rr::express_router_t<>;
 
+// ----------------- Server klasse -----------------
 class weatherInformationHandler {
 public:
-    explicit weatherInformationHandler(weatherStation_t &weather)
-        : m_weather(weather) {}
+    explicit weatherInformationHandler(weatherStation_t &weather) : m_weather(weather) {}
 
     auto on_weather_list(const restinio::request_handle_t &req, rr::route_params_t) const {
         auto resp = init_resp(req->create_response());
@@ -77,18 +69,17 @@ public:
             auto resp = init_resp(req->create_response(restinio::status_created()));
             resp.set_body(R"({"status": "added"})");
 
-            // Push ny data til WebSocket-klienter
+            // Push til WebSocket-klienter
             auto json_msg = json_dto::to_json(m_weather.back());
-            for (auto& [id, ws] : m_registry) {
-                ws->send_message(json_msg);
+            for (auto &[id, ws] : m_registry) {
+                ws->send_message(rws::message_t::builder().final().text(json_msg));
             }
 
             return resp.done();
-        }
-        catch (const std::exception &ex) {
+        } catch (const std::exception &ex) {
             return req->create_response(restinio::status_bad_request())
-                .set_body(std::string("Error: ") + ex.what())
-                .done();
+                      .set_body(std::string("Error: ") + ex.what())
+                      .done();
         }
     }
 
@@ -107,13 +98,12 @@ public:
             }
 
             return req->create_response(restinio::status_not_found())
-                .set_body("ID not found")
-                .done();
-        }
-        catch (...) {
+                      .set_body("ID not found")
+                      .done();
+        } catch (...) {
             return req->create_response(restinio::status_bad_request())
-                .set_body("Invalid data")
-                .done();
+                      .set_body("Invalid data")
+                      .done();
         }
     }
 
@@ -128,8 +118,8 @@ public:
         }
 
         return req->create_response(restinio::status_not_found())
-            .set_body("Not found")
-            .done();
+                  .set_body("Not found")
+                  .done();
     }
 
     auto on_weather_by_date(const restinio::request_handle_t &req, rr::route_params_t params) const {
@@ -156,17 +146,17 @@ public:
         return resp.done();
     }
 
-    auto on_live_update(const restinio::request_handle_t& req, rr::route_params_t) {
+    auto on_live_update(const restinio::request_handle_t &req, rr::route_params_t) {
         if (restinio::http_connection_header_t::upgrade == req->header().connection()) {
             auto wsh = rws::upgrade<traits_t>(*req, rws::activation_t::immediate,
                 [this](auto wsh, auto m) {
                     if (rws::opcode_t::text_frame == m->opcode()) {
-                        wsh->send_message(*m); // echo
-                    }
-                    else if (rws::opcode_t::connection_close_frame == m->opcode()) {
+                        wsh->send_message(*m);  // echo
+                    } else if (rws::opcode_t::connection_close_frame == m->opcode()) {
                         m_registry.erase(wsh->connection_id());
                     }
                 });
+
             m_registry.emplace(wsh->connection_id(), wsh);
             return restinio::request_accepted();
         }
@@ -187,8 +177,7 @@ private:
 
     template <typename RESP>
     static RESP init_resp(RESP resp) {
-        resp
-            .append_header("Server", "RESTinio WeatherServer")
+        resp.append_header("Server", "RESTinio WeatherServer")
             .append_header_date_field()
             .append_header("Content-Type", "application/json; charset=utf-8")
             .append_header("Access-Control-Allow-Origin", "*");
@@ -196,6 +185,7 @@ private:
     }
 };
 
+// ----------------- Server setup -----------------
 auto server_handler(weatherStation_t &weatherStation) {
     auto router = std::make_unique<router_t>();
     auto handler = std::make_shared<weatherInformationHandler>(std::ref(weatherStation));
@@ -214,15 +204,11 @@ auto server_handler(weatherStation_t &weatherStation) {
     return router;
 }
 
+// ----------------- main() -----------------
 int main() {
     using namespace std::chrono;
 
     try {
-        using traits_t = restinio::traits_t<
-            restinio::asio_timer_manager_t,
-            restinio::single_threaded_ostream_logger_t,
-            router_t>;
-
         weatherStation_t weatherStation{
             {1, 20240415, 1015, "Aarhus N", 13.692, 19.438, 13.1, 70}
         };
@@ -235,8 +221,7 @@ int main() {
                 .read_next_http_message_timelimit(10s)
                 .write_http_response_timelimit(1s)
                 .handle_request_timeout(1s));
-    }
-    catch (const std::exception &ex) {
+    } catch (const std::exception &ex) {
         std::cerr << "Error: " << ex.what() << std::endl;
         return 1;
     }
